af6017c16c548e46da5308e3605cd742383cadb7
any <153752169+dineshk042002@users.noreply.github.com>
2023-12-18 18:36:31 +0530

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { applyMiddleware, compose, createStore } from 'redux';
+import {applyMiddleware,compose} from 'redux';
+import {createStore} from 'redux';
 import Reducers from './Reducers';
 import thunk from 'redux-thunk';
 
-const store = createStore(Reducers, compose(applyMiddleware(thunk)));
-
+const store=createStore(Reducers,compose(applyMiddleware(thunk)))
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
   </Provider>
 );
 
